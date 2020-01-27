51152581320151da14b109ec404e8fb26086b4de
Lauren Hallau <lhallau@gmail.com>
2020-01-27 14:24:31 -0600

    updated readme with link

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Weather-Dashboard
 Homework #6: Server Side APIs
+
+### https://laurenhallau.github.io/Weather-Dashboard/
--- script.js
+++ script.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
         localStorage.setItem(city, location);//help with local storage of city entered
         var city = $("#city").val().trim();
         var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=e1a3479820563a2c503617a91d1ec1d3";
-        var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&mode=xml" + "&APPID=e1a3479820563a2c503617a91d1ec1d3";
         
         $.ajax({
             url: queryURL,
@@ -17,9 +16,6 @@ $(document).ready(function(){
           $("#cityName").html(cityName);
           console.log(cityName);
           localStorage.setItem(cityName, historyRow);
-
-          var time = (response.timezone);
-          $("#time").html(time);
           
           var icon = (response.weather[0].icon);
           $("#icon").html("<img src='http://openweathermap.org/img/w/" + icon + ".png' alt='Icon depicting current weather.'>")
@@ -29,6 +25,10 @@ $(document).ready(function(){
 
           var humidity = Math.round(response.main.humidity);
           $("#humidity").html("Humidity: " + humidity + "%");
+
+          var wind = (response.wind.speed);
+          $("#wind").html("Wind Speed: " + wind + " mph");
+
         })
         
     })
