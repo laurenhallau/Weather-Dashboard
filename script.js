0eeb9555b904c21c307472e1b55e6d5e9219cfa3
Lauren Hallau <lhallau@gmail.com>
2020-01-28 12:09:22 -0600

    five day forecast

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 $(document).ready(function(){
+
+    function forecast(searchCity){
+        var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&units=imperial" +"&APPID=e1a3479820563a2c503617a91d1ec1d3";
+        $.ajax({
+            type: "GET",
+            url: queryURL,
+        }).then(function(response){
+            
+            var forecastArr = response.list;
+            for (let i = 0; i < forecastArr.length; i++) {
+                console.log("test", forecastArr[i]);
+            //create a var called col=div add a class
+                
+            }
+        })
+    }
+
+
+
+
+
+
     $("#searchButton").on("click", function(event){
         event.preventDefault();
-        localStorage.setItem(city, location);//help with local storage of city entered
+        
         var city = $("#city").val().trim();
         var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=e1a3479820563a2c503617a91d1ec1d3";
-        
+        //local storage
+        localStorage.setItem("city", city);
+        console.log(localStorage);
+
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -15,7 +40,6 @@ $(document).ready(function(){
           var cityName = (response.name);
           $("#cityName").html(cityName);
           console.log(cityName);
-          localStorage.setItem(cityName, historyRow);
           
           var icon = (response.weather[0].icon);
           $("#icon").html("<img src='http://openweathermap.org/img/w/" + icon + ".png' alt='Icon depicting current weather.'>")
@@ -30,6 +54,7 @@ $(document).ready(function(){
           $("#wind").html("Wind Speed: " + wind + " mph");
 
         })
-        
+     forecast(city);   
     })
+
 })
